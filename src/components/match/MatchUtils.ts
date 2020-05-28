afefe3957ed9e31959255b6483ba8be0577c56f3
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-05-28 21:12:01 +0900

    add basic MatchUtils

---
+++ src/components/match/MatchUtils.ts
@@ -0,0 +1,23 @@
+import { Match, Team } from '../../models';
+
+/**
+ * initialize matchs
+ *
+ * @param teams number of players
+ */
+export const getMatches = (teams: Team[]): Match[] => {
+  const matches: Match[] = [];
+
+  return matches;
+};
+
+/**
+ * initialize organized matches
+ *
+ * @param matches
+ */
+export const getOrganizedMatches = (matches: Match[]) => {
+  const organizedMatches: Match[] = [];
+
+  return organizedMatches;
+};
