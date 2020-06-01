4bab8fd936307b3cae834e34e0beb1920dfbf8f2
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-06-01 16:07:06 +0900

    update player test

--- src/components/player/PlayerPanel.test.tsx
+++ src/components/player/PlayerPanel.test.tsx
@@ -2,13 +2,20 @@ import { render, screen } from '@testing-library/react';
 import React, { Suspense } from 'react';
 import { ThemeProvider } from 'react-jss';
 import { BrowserRouter } from 'react-router-dom';
-import { Player } from '../../models';
+import { Player, Stats } from '../../models';
 import { theme } from '../utils/Theme';
 import PlayerPanel from './PlayerPanel';
 
 test('renders without crashing', async () => {
   const player = new Player({
+    userid: 'test',
     name: 'Player1',
+    stats: new Stats({
+      numDraws: 0,
+      numLost: 0,
+      numMatches: 0,
+      numWon: 0,
+    }),
   });
 
   render(
