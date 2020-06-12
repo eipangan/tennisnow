2cbea48ce3fd70682800fa8b4c7f1da1fe09590a
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-06-12 22:26:48 +0900

    improve MatchPanel test

--- src/components/match/MatchPanel.test.tsx
+++ src/components/match/MatchPanel.test.tsx
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React, { Suspense } from 'react';
 import { ThemeProvider } from 'react-jss';
 import { BrowserRouter } from 'react-router-dom';
-import { getNewEvent, getNextMatch } from '../event/EventUtils';
+import { getNewEvent, getNextMatch, getPlayers } from '../event/EventUtils';
 import { theme } from '../utils/Theme';
 import MatchPanel from './MatchPanel';
 
@@ -13,25 +13,32 @@ jest.mock('react-i18next', () => ({
 
 test('render new without crashing', async () => {
   const event = getNewEvent();
-  const { players, matches } = event;
+  const players = getPlayers(event.id, 6);
+  const match = getNextMatch(event.id, players);
+  const matches = [match];
 
-  const match = getNextMatch(event);
-  if (players && matches && match) {
-    matches.push(match);
+  expect(event).toBeDefined();
+  expect(players).toBeDefined();
+  expect(match).toBeDefined();
+  expect(matches).toBeDefined();
 
-    if (matches.length > 0) {
-      render(
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <Suspense fallback={null}>
-              <MatchPanel
-                match={match}
-                players={players}
-              />
-            </Suspense>
-          </ThemeProvider>
-        </BrowserRouter>,
-      );
-    }
+  if (match && players) {
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Suspense fallback={null}>
+            <MatchPanel
+              match={match}
+              players={players}
+            />
+          </Suspense>
+        </ThemeProvider>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getAllByRole('button')).toHaveLength(3);
+    expect(screen.getByText('vs')).toBeInTheDocument();
+
+    // fireEvent.click(screen.getAllByRole('button')[0]);
   }
 });
