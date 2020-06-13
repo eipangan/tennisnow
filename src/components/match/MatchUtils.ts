108df94fd33b1719bc67d88988b006a276f50f04
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-06-13 11:58:10 +0900

    add deleteMatch() to MatchUtils

--- src/components/match/MatchUtils.ts
+++ src/components/match/MatchUtils.ts
@@ -2,11 +2,20 @@ import { DataStore } from 'aws-amplify';
 import { Match } from '../../models';
 
 /**
- * saveMatch
+ * delete match
  *
  * @param myMatch
  */
-// eslint-disable-next-line import/prefer-default-export
+export const deleteMatch = async (myMatch: Match) => {
+  console.log('deleteMatch', myMatch);
+  await DataStore.delete(myMatch);
+};
+
+/**
+ * save match
+ *
+ * @param myMatch
+ */
 export const saveMatch = async (myMatch: Match) => {
   console.log('saveMatch', myMatch);
   await DataStore.save(myMatch);
