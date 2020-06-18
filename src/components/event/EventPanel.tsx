b9a552be76cb7e4159bd9ed059fb45db96da3a7d
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-06-18 11:35:21 +0900

    extract players from DataStore directly

--- src/components/event/EventPanel.tsx
+++ src/components/event/EventPanel.tsx
@@ -6,7 +6,7 @@ import calendar from 'dayjs/plugin/calendar';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { createUseStyles, useTheme } from 'react-jss';
-import { Event, Match } from '../../models';
+import { Event, Match, Player } from '../../models';
 import MatchesList from '../match/MatchesList';
 import { saveMatch } from '../match/MatchUtils';
 import PlayersSummary from '../player/PlayersSummary';
@@ -48,18 +48,29 @@ const EventPanel = (props: EventPanelProps): JSX.Element => {
 
   const { event } = props;
   const [matches, setMatches] = useState<Match[]>();
+  const [players, setPlayers] = useState<Player[]>();
 
+  // update matches
   useEffect(() => {
     const fetchMatches = async () => {
-      const allMatches = await DataStore.query(Match);
-      const fetchedMatches = allMatches.filter((m) => m.eventID === event.id);
+      const fetchedMatches = await DataStore.query(Match, (m) => m.eventID('eq', event.id));
       setMatches(fetchedMatches);
     };
 
     fetchMatches();
-    const subscription = DataStore.observe(Match).subscribe(() => {
-      fetchMatches();
-    });
+    const subscription = DataStore.observe(Match).subscribe(() => fetchMatches());
+    return () => subscription.unsubscribe();
+  }, [event.id]);
+
+  // update players
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const fetchedPlayers = await DataStore.query(Player, (p) => p.eventID('eq', event.id));
+      setPlayers(fetchedPlayers);
+    };
+
+    fetchPlayers();
+    const subscription = DataStore.observe(Match).subscribe(() => fetchPlayers());
     return () => subscription.unsubscribe();
   }, [event.id]);
 
@@ -68,13 +79,13 @@ const EventPanel = (props: EventPanelProps): JSX.Element => {
       {dayjs(event.date).calendar()}
       <MatchesList
         matches={matches || []}
-        players={event.players || []}
+        players={players || []}
         extra={(
           <Button
             data-testid="add-match"
             icon={<PlusOutlined />}
             onClick={() => {
-              const newMatch = getNextMatch(event.id, event.players, matches);
+              const newMatch = getNextMatch(event.id, players, matches);
               if (newMatch) {
                 saveMatch(newMatch);
                 if (matches) {
@@ -91,7 +102,7 @@ const EventPanel = (props: EventPanelProps): JSX.Element => {
       />
       <div className={classes.eventPlayersSummary}>
         <PlayersSummary
-          players={event.players || []}
+          players={players || []}
           matches={matches || []}
         />
       </div>
