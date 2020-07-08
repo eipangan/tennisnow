6d0797a099fb0027b2e64c3142fb49357f679384
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-07-08 20:25:04 +0900

    test getPlayers first before getNextMatch

--- src/tests/EventUtils.test.ts
+++ src/tests/EventUtils.test.ts
@@ -17,6 +17,121 @@ DataStore.observe = jest.fn().mockImplementation(() => ({
 }));
 
 /**
+ * getPlayers() tests
+ */
+
+test('runs getPlayers() with just eventID parameter', () => {
+  const defaultNumPlayers = 6;
+  const players = getNewPlayers(event.id);
+
+  // players
+  expect(players).toBeDefined();
+  expect(players).not.toBeNull();
+  expect(players).toHaveLength(defaultNumPlayers);
+
+  // eventID --- required
+  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
+  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(defaultNumPlayers);
+
+  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
+  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
+
+  // index --- required
+  expect(players?.find((player) => player.index === 0)).toBeDefined();
+  expect(players?.find((player) => player.index === 1)).toBeDefined();
+  expect(players?.find((player) => player.index === 2)).toBeDefined();
+  expect(players?.find((player) => player.index === 3)).toBeDefined();
+  expect(players?.find((player) => player.index === 4)).toBeDefined();
+  expect(players?.find((player) => player.index === 5)).toBeDefined();
+  expect(players?.find((player) => player.index === 6)).toBeUndefined();
+
+  // name
+  expect(players?.find((player) => player.name === '')).toBeDefined();
+  expect(players?.filter((player) => player.name === '')).toHaveLength(defaultNumPlayers);
+  expect(players?.find((player) => player.name === '1')).toBeUndefined();
+
+  if (players) {
+    players.forEach((player) => {
+      expect(player).toBeDefined();
+      expect(player).not.toBeNull();
+
+      expect(player.id).toBeDefined();
+      expect(player.id).not.toBeNull();
+      expect(player.id.length).toBeGreaterThan(32);
+    });
+  }
+});
+
+test('runs getPlayers() with eventID, numPlayers=4 parameter', () => {
+  const numPlayers = 4;
+  const players = getNewPlayers(event.id, numPlayers);
+
+  // players
+  expect(players).toBeDefined();
+  expect(players).not.toBeNull();
+  expect(players).toHaveLength(numPlayers);
+
+  // eventID --- required
+  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
+  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(numPlayers);
+
+  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
+  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
+
+  // index --- required
+  expect(players?.find((player) => player.index === 0)).toBeDefined();
+  expect(players?.find((player) => player.index === 1)).toBeDefined();
+  expect(players?.find((player) => player.index === 2)).toBeDefined();
+  expect(players?.find((player) => player.index === 3)).toBeDefined();
+  expect(players?.find((player) => player.index === 4)).toBeUndefined();
+
+  // name
+  expect(players?.find((player) => player.name === '')).toBeDefined();
+  expect(players?.filter((player) => player.name === '')).toHaveLength(numPlayers);
+  expect(players?.find((player) => player.name === '1')).toBeUndefined();
+});
+
+test('runs getPlayers() with eventID, numPlayers=4, playersNames parameter', () => {
+  const numPlayers = 4;
+  const playerNames = ['P1', 'P2', 'P3'];
+  const players = getNewPlayers(event.id, numPlayers, playerNames);
+
+  // players
+  expect(players).toBeDefined();
+  expect(players).not.toBeNull();
+  expect(players).toHaveLength(numPlayers);
+
+  // eventID --- required
+  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
+  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(numPlayers);
+
+  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
+  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
+
+  // index --- required
+  expect(players?.find((player) => player.index === 0)).toBeDefined();
+  expect(players?.find((player) => player.index === 1)).toBeDefined();
+  expect(players?.find((player) => player.index === 2)).toBeDefined();
+  expect(players?.find((player) => player.index === 3)).toBeDefined();
+  expect(players?.find((player) => player.index === 4)).toBeUndefined();
+
+  // name
+  expect(players?.find((player) => player.name === '')).toBeDefined();
+  expect(players?.filter((player) => player.name === '')).toHaveLength(1);
+
+  expect(players?.find((player) => player.name === 'P1')).toBeDefined();
+  expect(players?.filter((player) => player.name === 'P1')).toHaveLength(1);
+
+  expect(players?.find((player) => player.name === 'P2')).toBeDefined();
+  expect(players?.filter((player) => player.name === 'P2')).toHaveLength(1);
+
+  expect(players?.find((player) => player.name === 'P3')).toBeDefined();
+  expect(players?.filter((player) => player.name === 'P3')).toHaveLength(1);
+
+  expect(players?.find((player) => player.name === 'P4')).toBeUndefined();
+});
+
+/**
  * getNewEvent() tests
  */
 
@@ -182,119 +297,4 @@ test('runs getNextMatch() with eventID, matches[1], players[6], parameter', asyn
 
   // owner
   expect(nextMatch?.owner).toBeUndefined();
-});
-
-/**
- * getPlayers() tests
- */
-
-test('runs getPlayers() with just eventID parameter', () => {
-  const defaultNumPlayers = 6;
-  const players = getNewPlayers(event.id);
-
-  // players
-  expect(players).toBeDefined();
-  expect(players).not.toBeNull();
-  expect(players).toHaveLength(defaultNumPlayers);
-
-  // eventID --- required
-  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
-  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(defaultNumPlayers);
-
-  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
-  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
-
-  // index --- required
-  expect(players?.find((player) => player.index === 0)).toBeDefined();
-  expect(players?.find((player) => player.index === 1)).toBeDefined();
-  expect(players?.find((player) => player.index === 2)).toBeDefined();
-  expect(players?.find((player) => player.index === 3)).toBeDefined();
-  expect(players?.find((player) => player.index === 4)).toBeDefined();
-  expect(players?.find((player) => player.index === 5)).toBeDefined();
-  expect(players?.find((player) => player.index === 6)).toBeUndefined();
-
-  // name
-  expect(players?.find((player) => player.name === '')).toBeDefined();
-  expect(players?.filter((player) => player.name === '')).toHaveLength(defaultNumPlayers);
-  expect(players?.find((player) => player.name === '1')).toBeUndefined();
-
-  if (players) {
-    players.forEach((player) => {
-      expect(player).toBeDefined();
-      expect(player).not.toBeNull();
-
-      expect(player.id).toBeDefined();
-      expect(player.id).not.toBeNull();
-      expect(player.id.length).toBeGreaterThan(32);
-    });
-  }
-});
-
-test('runs getPlayers() with eventID, numPlayers=4 parameter', () => {
-  const numPlayers = 4;
-  const players = getNewPlayers(event.id, numPlayers);
-
-  // players
-  expect(players).toBeDefined();
-  expect(players).not.toBeNull();
-  expect(players).toHaveLength(numPlayers);
-
-  // eventID --- required
-  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
-  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(numPlayers);
-
-  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
-  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
-
-  // index --- required
-  expect(players?.find((player) => player.index === 0)).toBeDefined();
-  expect(players?.find((player) => player.index === 1)).toBeDefined();
-  expect(players?.find((player) => player.index === 2)).toBeDefined();
-  expect(players?.find((player) => player.index === 3)).toBeDefined();
-  expect(players?.find((player) => player.index === 4)).toBeUndefined();
-
-  // name
-  expect(players?.find((player) => player.name === '')).toBeDefined();
-  expect(players?.filter((player) => player.name === '')).toHaveLength(numPlayers);
-  expect(players?.find((player) => player.name === '1')).toBeUndefined();
-});
-
-test('runs getPlayers() with eventID, numPlayers=4, playersNames parameter', () => {
-  const numPlayers = 4;
-  const playerNames = ['P1', 'P2', 'P3'];
-  const players = getNewPlayers(event.id, numPlayers, playerNames);
-
-  // players
-  expect(players).toBeDefined();
-  expect(players).not.toBeNull();
-  expect(players).toHaveLength(numPlayers);
-
-  // eventID --- required
-  expect(players?.find((player) => player.eventID === event.id)).toBeDefined();
-  expect(players?.filter((player) => player.eventID === event.id)).toHaveLength(numPlayers);
-
-  expect(players?.find((player) => player.eventID === generateUUID())).toBeUndefined();
-  expect(players?.filter((player) => player.eventID === generateUUID())).toHaveLength(0);
-
-  // index --- required
-  expect(players?.find((player) => player.index === 0)).toBeDefined();
-  expect(players?.find((player) => player.index === 1)).toBeDefined();
-  expect(players?.find((player) => player.index === 2)).toBeDefined();
-  expect(players?.find((player) => player.index === 3)).toBeDefined();
-  expect(players?.find((player) => player.index === 4)).toBeUndefined();
-
-  // name
-  expect(players?.find((player) => player.name === '')).toBeDefined();
-  expect(players?.filter((player) => player.name === '')).toHaveLength(1);
-
-  expect(players?.find((player) => player.name === 'P1')).toBeDefined();
-  expect(players?.filter((player) => player.name === 'P1')).toHaveLength(1);
-
-  expect(players?.find((player) => player.name === 'P2')).toBeDefined();
-  expect(players?.filter((player) => player.name === 'P2')).toHaveLength(1);
-
-  expect(players?.find((player) => player.name === 'P3')).toBeDefined();
-  expect(players?.filter((player) => player.name === 'P3')).toHaveLength(1);
-
-  expect(players?.find((player) => player.name === 'P4')).toBeUndefined();
 });
