b79caa163f41225369f6b187c0b1bad99bc180fb
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-07-09 16:06:04 +0900

    arrange as Event > Player > Match

--- src/tests/EventUtils.test.ts
+++ src/tests/EventUtils.test.ts
@@ -17,6 +17,33 @@ DataStore.observe = jest.fn().mockImplementation(() => ({
 }));
 
 /**
+ * getNewEvent() tests
+ */
+
+test('runs getNewEvent() as expected', () => {
+  expect(event).toBeDefined();
+  expect(event).not.toBeNull();
+
+  expect(event.id).toBeDefined();
+  expect(event.id).not.toBeNull();
+  expect(event.id.length).toBeGreaterThan(32);
+
+  expect(event.date).toBeDefined();
+  expect(event.date).not.toBeNull();
+  expect(event.date).toContain('T');
+  expect(event.date).toContain('Z');
+
+  // initially undefined parameters
+  expect(event.place).toBeUndefined();
+  expect(event.type).toBeUndefined();
+  expect(event.summary).toBeUndefined();
+  expect(event.details).toBeUndefined();
+  expect(event.matches).toBeUndefined();
+  expect(event.players).toBeUndefined();
+  expect(event.owner).toBeUndefined();
+});
+
+/**
  * getPlayers() tests
  */
 
@@ -129,33 +156,6 @@ test('runs getPlayers() with eventID, numPlayers=4, playersNames parameter', () 
   expect(players?.filter((player) => player.name === 'P3')).toHaveLength(1);
 
   expect(players?.find((player) => player.name === 'P4')).toBeUndefined();
-});
-
-/**
- * getNewEvent() tests
- */
-
-test('runs getNewEvent() as expected', () => {
-  expect(event).toBeDefined();
-  expect(event).not.toBeNull();
-
-  expect(event.id).toBeDefined();
-  expect(event.id).not.toBeNull();
-  expect(event.id.length).toBeGreaterThan(32);
-
-  expect(event.date).toBeDefined();
-  expect(event.date).not.toBeNull();
-  expect(event.date).toContain('T');
-  expect(event.date).toContain('Z');
-
-  // initially undefined parameters
-  expect(event.place).toBeUndefined();
-  expect(event.type).toBeUndefined();
-  expect(event.summary).toBeUndefined();
-  expect(event.details).toBeUndefined();
-  expect(event.matches).toBeUndefined();
-  expect(event.players).toBeUndefined();
-  expect(event.owner).toBeUndefined();
 });
 
 /**
