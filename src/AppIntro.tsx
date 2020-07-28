87938ca8e4d64ca23f6a99a9b85809665e342795
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-07-28 17:18:14 +0900

    disable MatchesList for now

--- src/AppIntro.tsx
+++ src/AppIntro.tsx
@@ -1,12 +1,9 @@
 import { Typography } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { createUseStyles, useTheme } from 'react-jss';
-import { getNewEvent, getNewPlayers, getNextMatch } from './EventUtils';
 import { ReactComponent as Signup } from './images/signup.svg';
 import { ReactComponent as Tennis } from './images/tennis.svg';
-import MatchesList from './MatchesList';
-import { Match } from './models';
 import { ThemeType } from './Theme';
 
 // initialize styles
@@ -31,36 +28,6 @@ const AppIntro = (): JSX.Element => {
 
   const { Title, Text } = Typography;
 
-  const event = getNewEvent();
-  const [matches, setMatches] = useState<Match[]>();
-  const players = getNewPlayers(event.id, 6, ['1', '2', '3', '4', '5', '6']);
-
-  useEffect(() => {
-    const fetchMatches = async () => {
-      const myMatches: Match[] = [];
-      let myNextMatch: Match | undefined;
-
-      const getMyNextMatch = async () => {
-        myNextMatch = await getNextMatch(event, myMatches, players);
-        if (myNextMatch) myMatches.push(myNextMatch);
-      };
-
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-      await getMyNextMatch();
-
-      setMatches(myMatches);
-    };
-
-    fetchMatches();
-  }, [event, players]);
-
   return (
     <div className={classes.appIntro}>
       <Title className={classes.title} level={3}>
@@ -78,10 +45,6 @@ const AppIntro = (): JSX.Element => {
       <Text>
         {t('intro.events.body1')}
       </Text>
-      <MatchesList
-        matches={matches || []}
-        players={players || []}
-      />
       <Text>
         {t('intro.events.body2')}
       </Text>
