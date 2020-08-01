552a1b53a8c0d18fed55ef679386141e69251cc6
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-08-01 18:49:26 +0900

    no need to get event from EventRoute

--- src/EventRoute.tsx
+++ src/EventRoute.tsx
@@ -1,12 +1,10 @@
 import { PageHeader } from 'antd';
-import React, { Suspense, useEffect, useState } from 'react';
+import React from 'react';
 import { createUseStyles, useTheme } from 'react-jss';
 import { useHistory } from 'react-router-dom';
 import EventButtons from './EventButtons';
 import EventPanel from './EventPanel';
-import { getEvent } from './EventUtils';
 import { ReactComponent as AppTitle } from './images/title.svg';
-import { Event } from './models';
 import { ThemeType } from './Theme';
 
 // initialize styles
@@ -50,47 +48,13 @@ const EventRoute = (props: any): JSX.Element => {
   const theme = useTheme();
   const classes = useStyles({ theme });
 
-  const [event, setEvent] = useState<Event>();
+  // get eventID from router
   const { match } = props;
   const { params } = match;
   const eventID = params.id;
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  /**
-   * Loader
-   */
-  const Loader = (): JSX.Element => {
-    if (!isLoading) return <></>;
-    return (
-      <div className="loader" />
-    );
-  };
-
-  useEffect(() => {
-    const fetchEvent = async (id: string) => {
-      setIsLoading(true);
-      const myEvent = await getEvent(id);
-      setEvent(myEvent);
-      setIsLoading(false);
-    };
-
-    fetchEvent(eventID);
-  }, [eventID]);
-
-  if (!event) {
-    return (
-      <PageHeader
-        className={classes.appHeader}
-        onBack={() => history.push('/')}
-        title={(<AppTitle />)}
-      />
-    );
-  }
-
   return (
     <>
-      <Loader />
       <PageHeader
         className={classes.appHeader}
         onBack={() => history.push('/')}
@@ -102,9 +66,7 @@ const EventRoute = (props: any): JSX.Element => {
           />,
         ]}
       />
-      <Suspense fallback={<div className="loader" />}>
-        <EventPanel eventID={eventID} />
-      </Suspense>
+      <EventPanel eventID={eventID} />
     </>
   );
 };
