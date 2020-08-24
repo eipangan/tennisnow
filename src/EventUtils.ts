dfb95b44e7d60397825c42a7da7f23875d40be6c
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-08-24 18:30:30 +0900

    remove getNextMatch from EventUtils

--- src/EventUtils.ts
+++ src/EventUtils.ts
@@ -1,7 +1,7 @@
 import { DataStore } from 'aws-amplify';
 import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
-import { Event, EventType, Match, MatchStatus, Player } from './models';
+import { Event, EventType, Match, Player } from './models';
 import { deletePlayer, savePlayer } from './PlayerUtils';
 
 // initialize dayjs
@@ -62,117 +62,6 @@ export const getNewEvent = (): Event => {
   });
 
   return newEvent;
-};
-
-/**
- * get next match
- *
- * @param eventID
- * @param matches
- * @param players
- */
-export const getNextMatch = async (
-  event: Event,
-  matches: Match[] | undefined = undefined,
-  players: Player[] | undefined = undefined,
-): Promise<Match | undefined> => {
-  const myEvent = event;
-  const myMatches = matches || await getMatches(myEvent.id);
-  const myPlayers = players || await getPlayers(myEvent.id);
-
-  /**
-   * get all matches in array of [p1, p2] format
-  */
-  const getAllMatches = () => myPlayers.flatMap((v, i) => myPlayers
-    .slice(i + 1)
-    .map((w) => ([v.index, w.index])));
-
-  /**
-   * get all matches that already done in array of [p1, p2] format
-   */
-  const getAllMatchesDone = () => myMatches.map((m) => {
-    if (!m.playerIndices) return undefined;
-    return ([m.playerIndices[0], m.playerIndices[1]]);
-  });
-
-  /**
-   * get potential players, size is always equal to number of players
-   */
-  const getPotentialPlayers = () => {
-    // calculate number of matches played
-    const numPlayed = myPlayers.map(() => 0);
-    myMatches.forEach((myMatch) => {
-      if (myMatch.playerIndices) {
-        myMatch.playerIndices.forEach((playerIndex) => {
-          numPlayed[playerIndex] += 1;
-        });
-      }
-    });
-
-    // get potential players
-    const potentialPlayers: number[] = [];
-    for (let i = 0; i <= myMatches.length; i += 1) {
-      myPlayers.forEach((player) => {
-        if (numPlayed[player.index] === i) {
-          potentialPlayers.push(player.index);
-        }
-      });
-    }
-
-    // return potential players
-    return potentialPlayers;
-  };
-
-  /**
-   * get potential matches
-   */
-  const getPotentialMatches = () => {
-    const allMatches = getAllMatches();
-    const allMatchesDone = getAllMatchesDone();
-
-    const potentialMatches = allMatches.filter((m) => JSON
-      .stringify(allMatchesDone)
-      .indexOf(JSON.stringify(m)) === -1);
-    return potentialMatches;
-  };
-
-  const eventID = myEvent.id;
-  const potentialPlayers = getPotentialPlayers();
-  const potentialMatches = getPotentialMatches();
-
-  if (potentialPlayers.length === 0) return undefined;
-  if (potentialMatches.length === 0 && myMatches.length > 0) {
-    // TODO: need to fix this logic
-    return (new Match({
-      eventID,
-      createdTime: dayjs().toDate().toISOString(),
-      playerIndices: [0, 1],
-      status: MatchStatus.NEW,
-    }));
-  }
-
-  // get next match
-  let nextMatch: Match | undefined;
-  const BreakException = {};
-  try {
-    potentialPlayers.forEach((p1) => {
-      potentialPlayers.forEach((p2) => {
-        if (JSON.stringify(potentialMatches).indexOf(JSON.stringify([p1, p2])) !== -1) {
-          nextMatch = new Match({
-            eventID,
-            createdTime: dayjs().toDate().toISOString(),
-            playerIndices: [p1, p2],
-            status: MatchStatus.NEW,
-          });
-          throw BreakException;
-        }
-      });
-    });
-  } catch (e) {
-    if (e !== BreakException) throw e;
-  }
-
-  return nextMatch;
 };
 
 /**
--- src/tests/MatchPanel.test.tsx
+++ src/tests/MatchPanel.test.tsx
@@ -30,28 +30,17 @@ DataStore.save = jest.fn().mockImplementation(() => ({
 
 test('render new without crashing', async () => {
   const players = getNewPlayers(event.id, 6);
-  const match = await getNextMatch(event, [], players);
 
   expect(event).toBeDefined();
   expect(players).toBeDefined();
-  expect(match).toBeDefined();
 
-  if (!match) fail('match undefined.');
   await act(async () => {
     render(
       <ThemeProvider theme={theme}>
         <Suspense fallback={null}>
-          <MatchPanel matchID={match.id} match={match} />
+          <MatchPanel matchID="ABC" />
         </Suspense>
       </ThemeProvider>,
     );
   });
-
-  expect(screen.getByTestId('player1')).toBeInTheDocument();
-  expect(screen.getByTestId('middle')).toBeInTheDocument();
-  expect(screen.getByTestId('player2')).toBeInTheDocument();
-
-  fireEvent.click(screen.getByTestId('player1'));
-  fireEvent.click(screen.getByTestId('middle'));
-  fireEvent.click(screen.getByTestId('player2'));
 });
