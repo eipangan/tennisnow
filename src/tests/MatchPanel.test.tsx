070c89fa328495b5901856c564e011936d745d11
Enrico Pangan <eipangan@Enricos-iMac.local>
2020-10-20 18:10:43 +0900

    cleanup MatchPanel test cases

--- src/tests/MatchPanel.test.tsx
+++ src/tests/MatchPanel.test.tsx
@@ -1,21 +1,18 @@
-import { act, render } from '@testing-library/react';
+import { prettyDOM, render } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
-import { DataStore } from 'aws-amplify';
 import React, { Suspense } from 'react';
 import { ThemeProvider } from 'react-jss';
 import useEvent from '../hooks/useEvent';
+import '../i18n';
 import MatchPanel from '../MatchPanel';
 import { theme } from '../Theme';
 import { getNewPlayers } from '../utils/EventUtils';
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({ t: (key: any) => key }),
+jest.mock('../PlayerPanel', () => ({
+  PlayerPanel: jest.fn(),
 }));
 
-DataStore.save = jest.fn().mockImplementation(() => ({
-}));
-
-test('render new without crashing', async () => {
+beforeAll(() => {
   const { result } = renderHook(() => useEvent());
   const { current } = result;
   const { event } = current;
@@ -24,14 +21,20 @@ test('render new without crashing', async () => {
 
   expect(event).toBeDefined();
   expect(players).toBeDefined();
+});
 
-  await act(async () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <Suspense fallback={null}>
-          <MatchPanel matchID="ABC" />
-        </Suspense>
-      </ThemeProvider>,
-    );
+describe('MatchPanel', () => {
+  describe('invalid matchID', () => {
+    it('should render without crashing', () => {
+      render(
+        <ThemeProvider theme={theme}>
+          <Suspense fallback={null}>
+            <MatchPanel matchID="ABC" />
+          </Suspense>
+        </ThemeProvider>,
+      );
+
+      expect(prettyDOM()).toBeDefined();
+    });
   });
 });
