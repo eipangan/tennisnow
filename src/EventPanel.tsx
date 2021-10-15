c2bb3d564fd187e1bd1eb520335bbbb14097fdb2
Enrico Pangan <eipangan@hotmail.com>
2021-10-15 16:48:16 +0900

    code cleanup

--- src/EventPanel.tsx
+++ src/EventPanel.tsx
@@ -1,11 +1,9 @@
 import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
 import React, { useContext } from 'react';
-import { createUseStyles, useTheme } from 'react-jss';
 import { EventContext } from './EventContext';
 import MatchesPanel from './MatchesPanel';
 import PlayersSummary from './PlayersSummary';
-import { ThemeType } from './Theme';
 
 // initialize dayjs
 dayjs.extend(calendar);
