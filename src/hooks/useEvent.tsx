0e12825ae893f0cdc1832e62f091c5d2abd3d393
Enrico Pangan <eipangan@hotmail.com>
2021-11-01 20:16:54 +0900

    cleanup useEvent

--- src/hooks/useEvent.tsx
+++ src/hooks/useEvent.tsx
@@ -2,8 +2,7 @@ import { DataStore } from 'aws-amplify';
 import dayjs from 'dayjs';
 import calendar from 'dayjs/plugin/calendar';
 import { useEffect, useState } from 'react';
-import { Event, EventType, Match, MatchStatus } from '../models';
-import { getPlayers } from '../utils/EventUtils';
+import { Event, EventType } from '../models';
 
 // initialize dayjs
 dayjs.extend(calendar);
@@ -24,113 +23,21 @@ const useEvent = (id?: string) => {
     return newEvent;
   });
 
-  // useEffect for "id"
+  // re-fetch event when id changes
   useEffect(() => {
     let mounted = true;
     const fetchEvent = async (myID: string) => {
       const fetchedEvent = await DataStore.query(Event, myID);
-      if (fetchedEvent && mounted) setEvent(fetchedEvent);
+      if (fetchedEvent && mounted) {
+        setEvent(fetchedEvent);
+      }
     };
 
     if (id) { fetchEvent(id); }
-    return () => { mounted = false; };
+    return () => {
+      mounted = false;
+    };
   }, [id]);
-
-  // get next match
-  const getNextMatch = async (): Promise<Match | undefined> => {
-    const myEvent = event;
-    if (!myEvent) return undefined;
-
-    const myMatches = await DataStore.query(Match, (m) => m.eventID('eq', myEvent.id));
-    const myPlayers = await getPlayers(myEvent.id);
-
-    // get all matches in array of [p1, p2] format
-    const getAllMatches = () => myPlayers.flatMap((v, i) => myPlayers
-      .slice(i + 1)
-      .map((w) => ([v.index, w.index])));
-
-    // get all matches that already done in array of [p1, p2] format
-    const getAllMatchesDone = () => myMatches.map((m) => {
-      if (!m.playerIndices) return undefined;
-      return ([m.playerIndices[0], m.playerIndices[1]]);
-    });
-
-    // get potential players, size is always equal to number of players
-    const getPotentialPlayers = () => {
-      // calculate number of matches played
-      const numPlayed = myPlayers.map(() => 0);
-      myMatches.forEach((myMatch) => {
-        if (myMatch.playerIndices) {
-          myMatch.playerIndices.forEach((playerIndex) => {
-            if (playerIndex) numPlayed[playerIndex] += 1;
-          });
-        }
-      });
-
-      // get potential players
-      const potentialPlayers: number[] = [];
-      for (let i = 0; i <= myMatches.length; i += 1) {
-        myPlayers.forEach((player) => {
-          if (numPlayed[player.index] === i) {
-            potentialPlayers.push(player.index);
-          }
-        });
-      }
-
-      // return potential players
-      return potentialPlayers;
-    };
-
-    /**
-      * get potential matches
-      */
-    const getPotentialMatches = () => {
-      const allMatches = getAllMatches();
-      const allMatchesDone = getAllMatchesDone();
-
-      const potentialMatches = allMatches.filter((m) => JSON
-        .stringify(allMatchesDone)
-        .indexOf(JSON.stringify(m)) === -1);
-      return potentialMatches;
-    };
-
-    const potentialPlayers = getPotentialPlayers();
-    const potentialMatches = getPotentialMatches();
-
-    if (potentialPlayers.length === 0) return undefined;
-    if (potentialMatches.length === 0 && myMatches.length > 0) {
-      // TODO: need to fix this logic
-      return (new Match({
-        eventID: myEvent.id,
-        createdTime: dayjs().toDate().toISOString(),
-        playerIndices: [0, 1],
-        status: MatchStatus.NEW,
-      }));
-    }
-
-    // get next match
-    let nextMatch: Match | undefined;
-    const BreakException = {};
-    try {
-      potentialPlayers.forEach((p1) => {
-        potentialPlayers.forEach((p2) => {
-          if (JSON.stringify(potentialMatches).indexOf(JSON.stringify([p1, p2])) !== -1) {
-            nextMatch = new Match({
-              eventID: myEvent.id,
-              createdTime: dayjs().toDate().toISOString(),
-              playerIndices: [p1, p2],
-              status: MatchStatus.NEW,
-            });
-            throw BreakException;
-          }
-        });
-      });
-    } catch (e) {
-      if (e !== BreakException) throw e;
-    }
-
-    return nextMatch;
-  };
 
   return { event };
 };
