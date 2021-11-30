4068f2f706d2926b3fcaac9fcbad1e71e1caa2fc
Enrico Pangan <eipangan@hotmail.com>
2021-11-30 16:56:40 +0900

    clean up tests

--- src/utils/test/PlayerUtils.test.tsx
+++ src/utils/test/PlayerUtils.test.tsx
@@ -1,10 +1,13 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useEvent from '../../hooks/useEvent';
+import dayjs from 'dayjs';
+import { Event, EventType } from '../../models';
 
 describe('PlayerUtils', () => {
+  const event = new Event({
+    date: dayjs().add(1, 'hour').startOf('hour').toDate()
+      .toISOString(),
+    type: EventType.DOUBLES_ROUND_ROBIN,
+  });
+
   it('should runs getPlayerName() as expected', async () => {
-    const { result } = renderHook(() => useEvent());
-    const { current } = result;
-    const { event } = current;
   });
 });
