dd73db97e34730c1581c91f84b8b16ed9bb159b0
Enrico Pangan <eipangan@hotmail.com>
2021-11-30 17:18:48 +0900

    test cleanup

--- src/utils/test/Utils.test.tsx
+++ src/utils/test/Utils.test.tsx
@@ -1,10 +1,12 @@
-import { renderHook } from '@testing-library/react-hooks';
-import useEvent from '../../hooks/useEvent';
+import dayjs from 'dayjs';
+import { Event, EventType } from '../../models';
 
 describe('Utils', () => {
   it('should runs utils as expected', async () => {
-    const { result } = renderHook(() => useEvent());
-    const { current } = result;
-    const { event } = current;
+    const event = new Event({
+      date: dayjs().add(1, 'hour').startOf('hour').toDate()
+        .toISOString(),
+      type: EventType.DOUBLES_ROUND_ROBIN,
+    });
   });
 });
