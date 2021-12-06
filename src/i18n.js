22289d73c266945e21cc3be437ddf3c635a7b854
Enrico Pangan <eipangan@hotmail.com>
2021-12-06 18:24:43 +0900

    load translation using http (instead of xhr)

--- src/i18n.js
+++ src/i18n.js
@@ -1,15 +1,16 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-// not like to use this?
+// don't want to use this?
 // have a look at the Quick start guide
-// for passing in lng and translations on ini
+// for passing in lng and translations on init
 
 i18n
-  // load translation using xhr -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-xhr-backend
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
   .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
@@ -20,8 +21,6 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    keySeparator: '.',
-    whitelist: ['en', 'ja'],
     debug: false,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
